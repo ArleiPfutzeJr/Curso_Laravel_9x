4f746906f3f9bfdaf49f221627ad8cbd1afd816c
root <root@DESKTOP-1ECG4GU>
2024-07-09 10:14:57 -0300

    - create and use controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
 Route::get('/', function () {
     return view('welcome');
 });
